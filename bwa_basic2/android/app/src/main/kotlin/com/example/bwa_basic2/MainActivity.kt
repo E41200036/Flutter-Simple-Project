0e416857d2467f623c952484b641c2235df92aff
E41200036_MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2021-12-05 17:49:21 +0700

    new proect

---
+++ bwa_basic2/android/app/src/main/kotlin/com/example/bwa_basic2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bwa_basic2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bwa_basic2/lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BasicWidget extends StatelessWidget {
+  const BasicWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple App'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('ini adalah alarm'),
+              ));
+            },
+            icon: const Icon(Icons.access_alarm),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.accessible_forward_sharp),
+            tooltip: 'Show tooltips',
+          ),
+        ],
+        backgroundColor: const Color(0xff2a2a2a),
+      ),
+    );
+  }
+}
